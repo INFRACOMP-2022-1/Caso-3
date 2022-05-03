710546dc0d56552904e5539ab354f7866d804568
Veronica Escobar <vescobars2001@gmail.com>
2022-05-02 19:31:58 -0500

    añadi el contenido de la clase de server para que cree los threads. Creo que de esa forma la clase server server esta lista (por que todo el trabajo va a estar de hecho en serverThread. Server solo se hace responsable de aceptar los llamados

--- src-server/src/server/Server.java
+++ src-server/src/server/Server.java
@@ -1,7 +1,9 @@
 package server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.spec.ECField;
 
 /**
  * Server
@@ -14,6 +16,14 @@ public class Server {
     // CONSTANTS
     //----------------------------------------------------------------------
 
+    /*
+    The port the server. Its the logical endpoint of the network connection that is used to exchange information between a server and a client. This is what the client socket will be attached to
+     */
+    //TODO: Definir que puerto vamos a usar por ahora puse uno random
+    public static final int PORT = 2022;
+
+
+
     //----------------------------------------------------------------------
     // ATTRIBUTES
     //----------------------------------------------------------------------
@@ -21,7 +31,19 @@ public class Server {
     /*
     This is the server socket. It's responsible for listening for any client that wants to establish a connection to the server. It will return a socket with the connection to the client.
      */
-    ServerSocket serverSocket;
+    public static ServerSocket serverSocket;
+
+
+    /*
+    This is the servers private key. K_S-
+     */
+    public long privateKeyServer;
+
+    /*
+    This is the servers public key. K_S+
+     */
+    public long publicKeyServer;
+
 
 
 
@@ -52,5 +74,33 @@ public class Server {
     //----------------------------------------------------------------------
     public static void main(String[] args){
         System.out.println("Im the server");
+
+
+        //This socket will hold the endpoint of the network connection with the client. It holds the clients direction and port that the server will be sending information to.
+        Socket socket = null;
+
+        //The server socket is created and attached to the given port
+        try{
+            serverSocket = new ServerSocket(PORT);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        //The server will be permanently listening for any incoming connection until its shut off.
+        //TODO: Hablar con Geovanny si hay algun problema con esto.
+        while(true){
+            try{
+                //Listens for a connection and if there is one it accepts it. This creates a socket that its tied to the client in such a way that the server can communicate with him.
+                socket = serverSocket.accept();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+            //Launches a new thread to deal with the client connection
+            //TODO: Activar esto cuando ya hallamos terminado la clase de server socket
+            //new ServerThread(socket).start();
+        }
     }
 }
