d3b1d90e2b941aeab30f8b8ffe45396dcf91f264
Veronica Escobar <vescobars2001@gmail.com>
2022-05-06 13:39:18 -0500

    Ya copie la clase de utils a el src-client para poder usarla

---
+++ src/src-client/src/Utils/ByteUtils.java
@@ -0,0 +1,78 @@
+package Utils;
+
+import java.nio.ByteBuffer;
+
+/**
+ * The byte utils class contains a range of methods that convert from a variety of
+ * data types (String,Long,Integer,etc) to Byte Arrays (byte[]) and vice versa.
+ *
+ * @author Veronica Escobar
+ * @author Santiago Vela
+ */
+public class ByteUtils {
+
+    /**
+     * There are no attributes to be initialized so there is no constructor needed. The compiler will provide a default constructor.
+     */
+
+    //----------------------------------------------------------------------
+    // METHODS
+    //----------------------------------------------------------------------
+
+
+    /**
+     * Transforms bytes to strings.
+     * In relation to sockets , this is normally done before writing on the socket.
+     * @param b is the byte array
+     * @return String corresponding to the given byte array
+     */
+    public static String byte2str(byte[] b)
+    {
+        // Encapsulation with hexadecimals
+        String ret = "";
+        for (int i = 0 ; i < b.length ; i++) {
+            String g = Integer.toHexString(((char)b[i])&0x00ff);
+            ret += (g.length()==1?"0":"") + g;
+        }
+        return ret;
+    }
+
+    /**
+     * Reads the chains and converts them to bytes.
+     * In relation to sockets, this is normally done after it's read from a socket
+     * @param ss the string that is to be converted
+     * @return A byte array , byte[] , of the corresponding string
+     */
+    public static byte[] str2byte( String ss)
+    {
+        // Encapsulation with hexadecimals
+        byte[] ret = new byte[ss.length()/2];
+        for (int i = 0 ; i < ret.length ; i++) {
+            ret[i] = (byte) Integer.parseInt(ss.substring(i*2,(i+1)*2), 16);
+        }
+        return ret;
+    }
+
+    /**
+     * Reads the long and converts them to bytes.
+     * @param ll the long that is to be converted
+     * @return A byte array , byte[] , of the corresponding long
+     */
+    public static byte[] longToBytes(long ll) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(ll);
+        return buffer.array();
+    }
+
+    /**
+     * Transforms bytes to long.
+     * @param b is the byte array
+     * @return Long corresponding to the given byte array
+     */
+    public static long bytesToLong(byte[] b) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.put(b);
+        buffer.flip();//need flip
+        return buffer.getLong();
+    }
+}
---
+++ src/src-client/src/Utils/Encryption.java
@@ -0,0 +1,86 @@
+package Utils;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+
+/**
+ * Class responsible for encrypting data with a variety of algorithms and encryption types.
+ * This class also contains the HMAC algorithm used to create a authentication certificate for a message.
+ *
+ * @author Veronica Escobar
+ * @author Santiago Vela
+ */
+public class Encryption {
+
+    /**
+     * There are no attributes to be initialized so there is no constructor needed. The compiler will provide a default constructor.
+     */
+
+    //----------------------------------------------------------------------
+    // METHODS
+    //----------------------------------------------------------------------
+
+
+    /**
+     * Encrypts a message using a given secret key (symmetric).
+     * @param unencryptedMessageBytes the unencrypted message in a byte array format.
+     * @param secretKey the secret key used to encrypt the message
+     * @return a byte array containing the encrypted message.
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidAlgorithmParameterException
+     * @throws InvalidKeyException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     */
+    //TODO: REVISAR CON GEOVANNY SI ESTO ESTA BIEN
+    public static byte[] encryptWithSymmetricKey(byte[] unencryptedMessageBytes, SecretKey secretKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        IvParameterSpec ivspec = new IvParameterSpec(iv);//TODO: Esta esto del IV bien, o me toca generarlo de una forma mas inteligente
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+        byte[] encryptedMessageBytes = cipher.doFinal(unencryptedMessageBytes);
+        return encryptedMessageBytes;
+    }
+
+    /**
+     * Encrypts the given message using the given private key.
+     * @param unencryptedMessageBytes the unencrypted message in the format of a byte array
+     * @param publicKey the public key used to encrypt the message
+     * @return the encrypted message in the format of a byte array
+     * @throws InvalidKeyException
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     */
+    public static byte[] encryptWithPrivateKey(byte[] unencryptedMessageBytes, PublicKey publicKey) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException {
+        //REFERENCE -> https://www.baeldung.com/java-rsa
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedMessageBytes = cipher.doFinal(unencryptedMessageBytes);
+        return encryptedMessageBytes;
+    }
+
+    /**
+     * The signature of a given message using HMAC. This is used to create the authentication certificate.
+     * @param messageBytes the message in a byte array format (the message shouldn´t be encrypted)
+     * @param secretKey the secret key used to sign the HMAC
+     * @return a byte array with the HMAC signature
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     */
+    //TODO: GENUINAMENTE NO SE SI ENTENDI BIEN COMO HACER ESTO Y SI ES LO DE CERTIFICADOS DE AUTENTICCION
+    public static byte[] signWithHMAC(byte[] messageBytes, SecretKey secretKey) throws NoSuchAlgorithmException, InvalidKeyException {
+        //TODO: No tengo muy claro si HMAC usa la llave LS o no
+        String hmacSHA256Algorithm = "HmacSHA256";
+
+        Mac mac = Mac.getInstance(hmacSHA256Algorithm);
+        mac.init(secretKey);
+        return mac.doFinal(messageBytes);
+    }
+}
