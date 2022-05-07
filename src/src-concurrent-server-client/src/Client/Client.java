93d766887a7b6bf43e990b339ec9a3aca2162773
Veronica Escobar <vescobars2001@gmail.com>
2022-05-07 18:34:51 -0500

    Cree un objeto de package status request para guardar los requets que hace el cliente y que quede mucho mas facil hacer pruebas

--- src/src-concurrent-server-client/src/Client/Client.java
+++ src/src-concurrent-server-client/src/Client/Client.java
@@ -3,14 +3,18 @@ package Client;
 import Utils.KeyGenerators;
 
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.KeySpec;
 import java.security.spec.RSAPublicKeySpec;
+import java.util.ArrayList;
 
 /**
  * Client class is responsible for initiating and managing all the client threads, one client corresponds to one thread.
@@ -43,6 +47,11 @@ public class Client {
      */
     private static int numberOfActiveClients;
 
+    /*
+    The file name for the file where the server public key has been stored
+     */
+    private static String publicKeyStorageFileName;
+
     //----------------------------------------------------------------------
     // CONSTRUCTOR
     //----------------------------------------------------------------------
@@ -51,10 +60,14 @@ public class Client {
      * The client constructor. It manages the creation of client threads.
      * Each client makes a request to the server, which will dispatch a thread in order to deal with his petition.
      * @param publicKeyStorageFileName the file where the servers public key is stored
-     * @param numberOfActiveClients the number of clients that are going to be making a petition to the server. It will also correspond to the number of created server threads to deal with the pettitions.
+     * @param numberOfActiveClients the number of clients that are going to be making a petition to the server. It will also correspond to the number of created server threads to deal with the petitions.
+     * @param
      */
-    public Client(String publicKeyStorageFileName,int numberOfActiveClients) throws IOException {
+    public Client(String publicKeyStorageFileName, int numberOfActiveClients, ArrayList<PackageStatusRequests> packageStatusRequestList) throws IOException {
         System.out.println("Im the client");
+
+        //Stores the file name where the public key is going to be retreived from
+        this.publicKeyStorageFileName = publicKeyStorageFileName;
 
         //Read the servers public key from the storage file
         serverPublicKey = readPrivateKeyFromFile();
@@ -70,8 +83,23 @@ public class Client {
     //----------------------------------------------------------------------
     // METHODS
     //----------------------------------------------------------------------
-    public PublicKey readPrivateKeyFromFile(String fileName) throws IOException, NoSuchAlgorithmException {
-        //TODO: Ver lo del taller
+
+    /**
+     * Reads the private key from the file where the server stored the serialized object.
+     * @return The server public key
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     * @throws ClassNotFoundException
+     */
+    public PublicKey readPrivateKeyFromFile() throws IOException, NoSuchAlgorithmException, ClassNotFoundException {
+        //Creates file input stream for the file with the public key
+        FileInputStream file = new FileInputStream(publicKeyStorageFileName);
+
+        //Creates a object input stream to be able to read from the file
+        ObjectInputStream objectInputStream = new ObjectInputStream(file);
+
+        //Retrieves the public key stored in the file and returns it
+        return (PublicKey) objectInputStream.readObject();
     }
 
 
---
+++ src/src-concurrent-server-client/src/StatusRequests/PackageStatusRequests.java
@@ -0,0 +1,53 @@
+package StatusRequests;
+
+public class PackageStatusRequests {
+    //----------------------------------------------------------------------
+    // ATTRIBUTES
+    //----------------------------------------------------------------------
+
+    /*
+    The username attached to the package that is being searched
+     */
+    public String username;
+
+    /*
+    The package id of the searched package
+     */
+    public int packageId;
+
+    //----------------------------------------------------------------------
+    // CONSTRUCTOR
+    //----------------------------------------------------------------------
+
+    /**
+     * Creates a package status request. It contains the username attached to a package and the package id of the searched package.
+     * @param username the username attached to the searched package
+     * @param packageId the package id of the searched package
+     */
+    PackageStatusRequests(String username,int packageId){
+        this.username = username;
+        this.packageId = packageId;
+    }
+
+    //----------------------------------------------------------------------
+    // METHODS
+    //----------------------------------------------------------------------
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public void setPackageId(int packageId) {
+        this.packageId = packageId;
+    }
+
+
+}
