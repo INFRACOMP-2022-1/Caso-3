ab9fef47cdd13cb1cacd14094fde2ab448896be1
Veronica Escobar <vescobars2001@gmail.com>
2022-05-07 21:22:53 -0500

    Cree una nueva clase para guardar lo de HMAC(authentication code) y lo del MessageDigest(hash)

--- src/src-concurrent-server-client/src/Utils/Encryption.java
+++ src/src-concurrent-server-client/src/Utils/Encryption.java
@@ -6,7 +6,6 @@ import java.security.*;
 
 /**
  * Class responsible for encrypting data with a variety of algorithms and encryption types.
- * This class also contains the HMAC algorithm used to create a authentication certificate for a message.
  *
  * @author Veronica Escobar
  * @author Santiago Vela
@@ -65,48 +64,6 @@ public class Encryption {
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
             byte[] encryptedMessageBytes = cipher.doFinal(unencryptedMessageBytes);
             return encryptedMessageBytes;
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * The signature of a given message using HMAC. This is used to create the authentication certificate.
-     * @param messageBytes the message in a byte array format (the message shouldn´t be encrypted)
-     * @param secretKey the secret key used to sign the HMAC
-     * @return a byte array with the HMAC signature
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeyException
-     */
-    public static byte[] signWithHMAC(byte[] messageBytes, SecretKey secretKey){
-        try{
-            String hmacSHA256Algorithm = "HmacSHA256";
-
-            Mac mac = Mac.getInstance(hmacSHA256Algorithm);
-            mac.init(secretKey);
-            return mac.doFinal(messageBytes);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * Calculates a messages digest.
-     * A Message digest is basically a secure one way hash function that takes a byte array and outputs a fixed length hash value.
-     * @param messageBytes
-     * @return a byte array containing the produced hash, the digest
-     * @throws NoSuchAlgorithmException
-     */
-    public static byte[] getMessageDigest(byte[] messageBytes){
-        try{
-            String algorithm = "SHA-256";
-            MessageDigest digest = MessageDigest.getInstance(algorithm);
-            digest.update(messageBytes);
-            return digest.digest();
         }
         catch (Exception e){
             e.printStackTrace();
---
+++ src/src-concurrent-server-client/src/Utils/HashingAndAuthCodes.java
@@ -0,0 +1,62 @@
+package Utils;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Class responsible for implementing a variety of algorithms for hash functions and authentication codes.
+ */
+public class HashingAndAuthCodes {
+    /**
+     * There are no attributes to be initialized so there is no constructor needed. The compiler will provide a default constructor.
+     */
+
+    //----------------------------------------------------------------------
+    // METHODS
+    //----------------------------------------------------------------------
+
+    /**
+     * The signature of a given message using HMAC. This is used to create the authentication certificate.
+     * @param messageBytes the message in a byte array format (the message shouldn´t be encrypted)
+     * @param secretKey the secret key used to sign the HMAC
+     * @return a byte array with the HMAC signature
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     */
+    public static byte[] signWithHMAC(byte[] messageBytes, SecretKey secretKey){
+        try{
+            String hmacSHA256Algorithm = "HmacSHA256";
+
+            Mac mac = Mac.getInstance(hmacSHA256Algorithm);
+            mac.init(secretKey);
+            return mac.doFinal(messageBytes);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Calculates a messages digest.
+     * A Message digest is basically a secure one way hash function that takes a byte array and outputs a fixed length hash value.
+     * @param messageBytes
+     * @return a byte array containing the produced hash, the digest
+     * @throws NoSuchAlgorithmException
+     */
+    public static byte[] getMessageDigest(byte[] messageBytes){
+        try{
+            String algorithm = "SHA-256";
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            digest.update(messageBytes);
+            return digest.digest();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
